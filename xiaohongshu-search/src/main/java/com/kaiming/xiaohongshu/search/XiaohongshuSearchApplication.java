9ff8c3ffa4994e61f71187badd93859b3ce4a312
gongkaiming <12919034+gongkaiming@user.noreply.gitee.com>
2025-07-16 11:04:04 +0800

    搜索服务预搭建开发

---
+++ xiaohongshu-search/src/main/java/com/kaiming/xiaohongshu/search/XiaohongshuSearchApplication.java
@@ -0,0 +1,21 @@
+package com.kaiming.xiaohongshu.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: XiaohongshuSearchApplication
+ * Package: com.kaiming.xiaohongshu.search
+ * Description:
+ *
+ * @Auther gongkaiming
+ * @Create 2025/7/16 10:53
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class XiaohongshuSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuSearchApplication.class, args);
+    }
+}
